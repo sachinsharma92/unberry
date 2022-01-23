aea8d571510219d991bc0eb37271e048367f33cb
Sachin Sharma <sachin@gryt.fit>
2022-01-23 12:45:13 +0530

    REMOVE: extra files

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,28 +1,13 @@
 import React, {  }  from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginScreen from './pages/auth';
-import OtpVerifyScreen from './pages/auth/otpVerify';
-import MyDnaScreen from './pages/myDna';
-import MyDnaDetailScreen from './pages/myDna/details';
-import PositionScreen from './pages/positions';
-import PositionDetailScreen from './pages/positions/details';
-import NewPositionScreen from './pages/positions/newPosition';
-import ProfileScreen from './pages/profile';
+import HomeScreen from './pages/home';
 
 const AppRouter = () => {
-
   return (
     <div className="main-router-page">
       <Router>
         <Routes>
-          <Route path="/" element={<LoginScreen />} />
-          <Route path="/otp-verify" element={<OtpVerifyScreen />} />
-          <Route path="/position" element={<PositionScreen />} />
-          <Route path="/position-details" element={<PositionDetailScreen />} />
-          <Route path="/profile" element={<ProfileScreen />} />
-          <Route path="/add-new-position" element={<NewPositionScreen />} />
-          <Route path="/my-dna" element={<MyDnaScreen />} />
-          <Route path="/my-dna-detail" element={<MyDnaDetailScreen />} />
+          <Route path="/" element={<HomeScreen />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Styles
+import './styles.scss';
+
+export default function HomeScreen() {
+  return (
+    <div className='home-section-style'>
+      
+    </div>
+  )
+}
