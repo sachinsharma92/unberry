507c03cc62d8498cd0cc713764ed48296e2871c5
Sachin Sharma <sachin@gryt.fit>
2022-01-25 00:19:31 +0530

    ADD: new sections

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,15 +2,52 @@ import { Button, Col, Row } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LayoutPrimary from '../../common/layoutPrimary';
+
+// Images
 import logoFooter from "../../assets/logo-brown.svg"
 import illustrationLine from "../../assets/illustration/line-orange.svg";
-
 import potentialImage from "../../assets/icons/potential.svg";
-import hiresImage from "../../assets/icons/hires.svg"
-import bestfitImage from "../../assets/icons/best-fit.svg"
+import hiresImage from "../../assets/icons/hires.svg";
+import bestfitImage from "../../assets/icons/best-fit.svg";
+import gameImage from "../../assets/illustration/games.svg";
+import img1 from "../../assets/icons/img1.svg";
+import img2 from "../../assets/icons/img2.svg";
+import img3 from "../../assets/icons/img3.svg";
+import img4 from "../../assets/icons/img4.svg";
+import videoImage1 from "../../assets/video-img1.png";
+import videoImage2 from "../../assets/video-img2.jpg";
+import profile1 from "../../assets/icons/profile1.svg";
+import profile2 from "../../assets/icons/profile2.svg";
+import profile3 from "../../assets/icons/profile3.svg";
+import profile4 from "../../assets/icons/profile4.svg";
+import workImg1 from "../../assets/icons/work-img1.svg";
+import workImg2 from "../../assets/icons/work-img2.svg";
+import workImg3 from "../../assets/icons/work-img3.svg";
+
 
 // Styles
 import './styles.scss';
+
+const mediaboxData = [
+  {
+    number: '1',
+    title: 'We create your custom screening benchmarks for assesment',
+    description : 'To find out what ‘good’ looks like in each role within the company.',
+    imgUrl: workImg1,
+  },
+  {
+    number: '2',
+    title: 'Candidates play remotely and get assessed immediately',
+    description : 'A link with the automated Unberry gameplay takes care of everything.',
+    imgUrl: workImg2,
+  },
+  {
+    number: '3',
+    title: 'You get detailed reports with relevant, predictive insights',
+    description : 'To find out what ‘good’ looks like in each role within the company.',
+    imgUrl: workImg3,
+  }
+]
 
 export default function HomeScreen() {
   return (
@@ -83,66 +120,153 @@ export default function HomeScreen() {
         </section>
 
         {/* ======= Section ======= */}
-        <section className='section-style'>
+        <section className='section-style product-section'>
           <div className='menu-section'>
             <Link to="">02 The product</Link>
           </div>
           <div className='content-section'>
-            <Row>
-              <Col sm={20}>
-                <div className='heading-section'>
-                  <h1 className='title1'>Bury Biases</h1>
-                  <h1 className='title1'>Unbury Potential</h1>
+            <div className='sec-first'>
+              <Row gutter={50} className='h-100'>
+                <Col sm={17}>
+                  <div className='heading-section'>
+                    <h1 className='description'>Meet Unberry</h1>
+                    <h1 className='title1'>Game based assessments for high performing teams</h1>
+                  </div>
+                </Col>
+                <Col sm={7}>
+                  <img src={gameImage} className='product-img1' alt="" />
+                </Col>
+              </Row>
+            </div>
+
+            <Row gutter={50}>
+              <Col sm={17}>
+                <div className='video-image'>
+                  <img src={videoImage1} className='img-full' alt="" />
                 </div>
               </Col>
-              <Col sm={4}>
-                <Button type="primary">Book Demo</Button>
+              <Col sm={7}>
+                <div className="img-list">
+                  <div className='list'>
+                    <img src={img1} className='img-icon' alt="" />
+                    <p className='description'>Dynamic Scenarios & Authentic responses</p>
+                  </div>
+                  <div className='list'>
+                    <img src={img2} className='img-icon' alt="" />
+                    <p className='description'>Beyond consciously self reported data</p>
+                  </div>
+                  <div className='list'>
+                    <img src={img3} className='img-icon' alt="" />
+                    <p className='description'>Exhaustive & Extensive Data Points</p>
+                  </div>
+                  <div className='list'>
+                    <img src={img4} className='img-icon' alt="" />
+                    <p className='description'>Based on validated principles of psychology</p>
+                  </div>
+                </div>
               </Col>
             </Row>
-            <p className='description'>Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retail and delelop talent</p>
           </div>
         </section>
 
         {/* ======= Section ======= */}
-        <section className='section-style'>
-          <div className='menu-section'>
-            <Link to="">03 The product</Link>
-          </div>
-          <div className='content-section'>
-            <Row>
-              <Col sm={20}>
-                <div className='heading-section'>
-                  <h1 className='title1'>Bury Biases</h1>
-                  <h1 className='title1'>Unbury Potential</h1>
-                </div>
-              </Col>
-              <Col sm={4}>
-                <Button type="primary">Book Demo</Button>
-              </Col>
-            </Row>
-            <p className='description'>Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retail and delelop talent</p>
-          </div>
-        </section>
-
-        {/* ======= Section ======= */}
-        <section className='section-style'>
+        <section className='section-style product-section-2'>
           <div className='menu-section'>
             <Link to="">04 The Competency Traits</Link>
           </div>
+          <div className='content-section'>
+            <div className='first-section'>
+              <Row gutter={50}>
+                <Col sm={17}>
+                  <div className='heading-section'>
+                    <div className='text-sec'>
+                      <p className='description'>The Competency Traits</p>
+                      <h1 className='title1'>New age traits for modern workplace traits</h1>
+                    </div>
+                  </div>
+                </Col>
+                <Col sm={7}>
+                  <div className='video-image'>
+                    <img src={videoImage2} className='product-img1' alt="" />
+                  </div>
+                </Col>
+              </Row>
+            </div>
+
+            <div className='second-section'>
+              <Row gutter={50} className='h-100'>
+                <Col sm={17}>
+                  <div className='profile-boxes'>
+                    <div className='profile-box-style'>
+                      <img src={profile1} className='img-icon' alt="" />
+                      <div className='text-sec'>
+                        <h4 className='title4'>Cognitive Profile</h4>
+                        <p className='description'>How they process information & knowledge mentally</p>
+                      </div>
+                    </div>
+                    <div className='profile-box-style'>
+                      <img src={profile2} className='img-icon' alt="" />
+                      <div className='text-sec'>
+                        <h4 className='title4'>Thinking Profile</h4>
+                        <p className='description'>How they learn concepts & organise their thoughts </p>
+                      </div>
+                    </div>
+                    <div className='profile-box-style'>
+                      <img src={profile3} className='img-icon' alt="" />
+                      <div className='text-sec'>
+                        <h4 className='title4'>Intelligence Profile</h4>
+                        <p className='description'>How they isolate and navigate complex problems</p>
+                      </div>
+                    </div>
+                    <div className='profile-box-style'>
+                      <img src={profile4} className='img-icon' alt="" />
+                      <div className='text-sec'>
+                        <h4 className='title4'>Behavioural Profile</h4>
+                        <p className='description'>How they control and conduct themselves </p>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+
+                <Col sm={7}>
+                  <div className='boxes-description'>
+                    <p className='description'>We report cognitive and personality traits to predict real-world outcomes, all based on neuroscience and psychology principles.</p>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </div>
+        </section>
+
+        {/* ======= Section ======= */}
+        <section className='section-style how-works'>
+          <div className='menu-section'>
+            <Link to="">05 How it works</Link>
+          </div>
 
           <div className='content-section'>
-            <Row>
-              <Col sm={20}>
-                <div className='heading-section'>
-                  <h1 className='title1'>Bury Biases</h1>
-                  <h1 className='title1'>Unbury Potential</h1>
+            <div className='heading-section'>
+              <h1 className='title1 text-gradient'>“Here's how Unberry works and it’s simply a game changer”</h1>
+            </div>
+
+            <div className='media-section'>
+              {mediaboxData.map((item) => (
+                <div className='media-box'>
+                  <div className='number'>
+                    {item.number}
+                  </div>
+                  <div className='title'>
+                    {item.title}
+                  </div>
+                  <div className='description'>
+                    {item.description}
+                  </div>
+                  <div className='img-sec'>
+                  <img src={item.imgUrl} alt="" />
+                  </div>
                 </div>
-              </Col>
-              <Col sm={4}>
-                <Button type="primary">Book Demo</Button>
-              </Col>
-            </Row>
-            <p className='description'>Combining Game technology, Data science, & Neuroscience to disrupt the way companies recruit, retail and delelop talent</p>
+              ))}
+            </div>
           </div>
         </section>
 
