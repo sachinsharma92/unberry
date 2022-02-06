733def5127cce56071758bcbf1cd29355baf55bf
Sachin Sharma <sachin@gryt.fit>
2022-02-06 19:38:21 +0530

    FIX: some major issues and add blog detail page

--- src/pages/blogDetail/index.js
+++ src/pages/blogDetail/index.js
@@ -1,31 +1,39 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import './styles.scss'
 
 const BlogDetail = (props) => {
-    const {pathname} = useLocation()
+
+    const { pathname } = useLocation()
     let pathArr = pathname?.split('/')
-    let id =  pathArr[pathArr?.length - 1]
+    let id = pathArr[pathArr?.length - 1]
 
     const [data, setData] = useState({})
 
-    useEffect(()=>{
-        axios.get(`https://cms-api.unberry.com/api/v1/collection/${id}`).then(res=>{
+    useEffect(() => {
+        axios.get(`https://cms-api.unberry.com/api/v1/collection/${id}`).then(res => {
             setData(res?.data?.data)
-        }).catch(e=>{
+        }).catch(e => {
             console.log('blog detail err', e)
         })
-    },[])
-  return (
-    <div className='blog-detail-section'>
-        <div className='blog-layout'>
-        <h2 className='title2'>{data?.title}</h2>
-        <img className='img-blog' src={data?.mainPictureURL} alt={data?.title} />
-        <p className='description'>{data?.description}</p>
+    }, [])
+
+    // BacK function
+    const navigate = useNavigate();
+
+    return (
+        <div className='blog-detail-section'>
+            <div className='blog-layout'>
+                <a className='btn-back' onClick={() => navigate(-1)}>Back</a>
+                <div>
+                    <h2 className='title2'>{data?.title}</h2>
+                    <img className='img-blog' src={data?.mainPictureURL} alt={data?.title} />
+                    <p className='description'>{data?.description}</p>
+                </div>
+            </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default BlogDetail;
