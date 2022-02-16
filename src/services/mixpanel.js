4001cb328422c2e93e63e10704688a35df485301
Rajat Yadav <rajatyadav05.ry@gmail.com>
2022-02-16 23:25:14 +0530

    mixpanel token change

--- src/services/mixpanel.js
+++ src/services/mixpanel.js
@@ -1,5 +1,5 @@
 import mixpanel from 'mixpanel-browser';
-mixpanel.init('8dc2647a85fb3d3d87fc43bfd38daf2f', {debug: true});
+mixpanel.init('a84d5b5d2c9901eff738bc0a5ff7ed09', {ignore_dnt: true});
 let actions = {
   track: (name, props) => {
     mixpanel.track(name, props);
